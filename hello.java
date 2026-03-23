99fc2425fa980b09a169cdebbcf614a07d320b3c
Shakthivel <shakthivelgs2007@gmail.com>
2026-03-23 12:10:33 +0530

    Fix

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args) {
         String name = args[0];
-        System.out.println("Hello" + name + "!");
+        System.out.println("Hello " + name + "!");
     }
 }
