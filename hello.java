395d5842301a18e3a4ee80d66c188b7d72f7f2af
Shakthivel <shakthivelgs2007@gmail.com>
2026-03-24 13:57:53 +0530

    Updated dev branch for UC3

--- hello.java
+++ hello.java
@@ -1,6 +1,12 @@
 public class hello {
     public static void main(String[] args) {
-        String name = args[0];
+
+        String name = "World"; // Default case
+
+        if (args.length > 0) { // Checks for a command line input
+        String name = args[0]; 
+        }
+
         System.out.println("Hello " + name + "!");
     }
 }
