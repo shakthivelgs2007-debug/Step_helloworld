80d781eaf5a5aca770cc3f47c3012077420e7705
Shakthivel <shakthivelgs2007@gmail.com>
2026-03-24 14:31:07 +0530

    updating dev

--- hello.java
+++ hello.java
@@ -1,12 +1,24 @@
 public class hello {
     public static void main(String[] args) {
 
-        String name = "World"; // Default case
+        String name;
 
-        if (args.length > 0) { // Checks for a command line input
-        String name = args[0]; 
+        if (args.length == 0) { //incase user gives no input
+            name = "World";
+        } else {
+            StringBuilder nameBuilder = new StringBuilder(); // to join the names together into one string
+
+            for (int i = 0; i < args.length; i++) { // cycling through every name
+                nameBuilder.append(args[i]);
+
+                if (i < args.length - 1) {
+                    nameBuilder.append(", "); // Adding a comma between each name
+                }
+            }
+
+            name = nameBuilder.toString(); // assigning the finished string to 'name'
         }
 
-        System.out.println("Hello " + name + "!");
+        System.out.println("Hello " + name);
     }
 }
